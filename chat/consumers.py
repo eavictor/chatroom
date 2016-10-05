5d860d74997eb5f22adb813790080df67643c7df
eavictor <eavictor@me.com>
2016-10-05 17:49:02 +0800

    Client --> Server success, Server --> Other Client still failed

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,21 +1,19 @@
-from channels import Group
+from channels import Group, Channel
 from channels.sessions import channel_session
 
 
 @channel_session
-def ws_add(message, room):
-    Group('chat-%s' % room).add(message.reply_channel)
-    message.channel_session['room'] = room
+def ws_add(message):
+    Group('chat').add(message.reply_channel)
+    Group('chat').send({'text': 'Hello'})
 
 
 @channel_session
 def ws_echo(message):
-    room = message.channel_session['room']
-    Group('chat-%s' % room).send({
-        'text': message.content['text'],
-    })
+    Group('chat').send({'text': message.content['text']})
+    Channel('chat').send({'text': message.content['text']})
 
 
 @channel_session
-def ws_remove(message, room):
-    Group('chat-%s' % room).discard(message.reply_channel)
+def ws_remove(message):
+    Group('chat').discard(message.reply_channel)
